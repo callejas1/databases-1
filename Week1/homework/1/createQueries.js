593b881c0b2e77bf4f1ff83805318ba1487bef72
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2020-12-20 14:58:51 +0100

    Added end method to end connection and removed database inside connection object

--- Week1/homework/1/createQueries.js
+++ Week1/homework/1/createQueries.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const mysql = require('process-nextick-args/node_modules/mysql');
+const mysql = require('mysql');
 
 // Create connection
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'Password!',
-  database: 'meetup',
 });
 
 // Connect
@@ -65,3 +64,5 @@ createQuery(
 createQuery(
   "INSERT INTO Meeting(meeting_no, meeting_title, starting_time, ending_time, room_no) VALUES(1, 'Data analysis', '2020-12-28 13:00:00', '2020-12-28 14:00:00', 101), (2, 'Risk management', '2021-01-04 10:00:00', '2021-01-04 11:30:00', 119), (3, 'Digital Go', '2021-03-03 15:30:00', '2020-03-03 15:45:00', 35), (4, 'Web Prospect', '2020-02-23 12:15:00', '2020-02-23 13:00:00', 01), (5, 'Digital Core', '2020-03-19 14:45:00', '2020-03-19 15:45:00', 20)",
 );
+
+connection.end();
