4120faf51e4733892b1fa8f2ae446ea09e6df4d1
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-01-31 01:20:31 +0100

    Removed unnecessary params

--- Week3/homework/SQL-Injection/prevent-injection.js
+++ Week3/homework/SQL-Injection/prevent-injection.js
@@ -19,10 +19,10 @@ conn.connect((error) => {
 
 // Give an example of a value that can be passed as name and code that would take advantage of SQL-injection and (fetch all the records in the database)
 
-// function getPopulation(Country, name, code, cb) {
+// function getPopulation(name, code, cb) {
 //   // assuming that connection to the database is established and stored as conn
 //   conn.query(
-//     `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = '${code}'`,
+//     `SELECT Population FROM Country WHERE Name = '${name}' and code = '${code}'`,
 //     function (err, result) {
 //       // if (err) cb(err);
 //       // if (result.length == 0) cb(new Error('Not found'));
@@ -32,18 +32,18 @@ conn.connect((error) => {
 // }
 // // If query "SELECT Population FROM Country WHERE Name = 'Nicaragua' AND code = 'NIC' OR 1=1" is entered in the terminal || MySQL workbench then 239 rows are displayed in the results set.
 // // Does not display the same when done in Node.js. Thoughts?
-// getPopulation('Country', 'Nicaragua', "'NIC' OR 1=1", (err, result) => {
+// getPopulation('Nicaragua', '"NIC" OR 1=1', (err, result) => {
 //   if (err) {
 //     console.log(err);
 //   }
 //   console.log(`Population: ${result}`);
 // });
 
-// Rewrite the function so that it is no longer vulnerable to SQL injection
-// https://github.com/mysqljs/mysql#preparing-queries
+Rewrite the function so that it is no longer vulnerable to SQL injection
+https://github.com/mysqljs/mysql#preparing-queries
 function getPopulation(name, code, cb) {
   conn.query(
-    `SELECT Population FROM Country WHERE Name = ? and code = ?`,
+    'SELECT Population FROM Country WHERE Name = ? and code = ?',
     [name, code],
     (err, result) => {
       if (err) {
